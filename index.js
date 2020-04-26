bb6b7150b9c4e1036c424777d07bdad47547dc63
Meredith Reece <mardebugger@gmail.com>
2020-04-26 03:17:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,3 @@ function loadIssues() {
 var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
 var result = template(issue);
 document.getElementsByTagName("main")[0].innerHTML += result;
-
