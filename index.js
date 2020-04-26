f0ef71ef5157fce92ea0223dd052dc5a743e2071
Meredith Reece <mardebugger@gmail.com>
2020-04-26 02:59:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function loadIssue() {
+  var issue = {
+    "body": "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team.",
+    "created_at": "2016-03-31 16:23:13 UTC",
+    "comments_count": 0,
+    "id": 144948778,
+    "number": 7,
+    "state": "closed",
+    "url": "https://api.github.com/repos/learn-co-curriculum/basic-sinatra-forms-lab/issues/7"
+  }
 
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issue);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
